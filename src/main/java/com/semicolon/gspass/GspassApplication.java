8d30f6520ac9ad38aa87691f4a1c93ce69397d75
leeseojune <sung07288346@gmail.com>
2021-05-20 23:46:35 +0900

    📑 :: [Project] project init

---
+++ src/main/java/com/semicolon/gspass/GspassApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.gspass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GspassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GspassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/gspass/GspassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.gspass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GspassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
