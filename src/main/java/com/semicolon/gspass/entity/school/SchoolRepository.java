ddc6897fa0ee4fd88a3d728170e7d098268a65b3
leeseojune <sung07288346@gmail.com>
2021-05-25 22:31:29 +0900

    ⚡ :: [School] DAO 작성

--- src/main/java/com/semicolon/gspass/entity/school/SchoolRepository.java
+++ src/main/java/com/semicolon/gspass/entity/school/SchoolRepository.java
@@ -1,4 +1,8 @@
 package com.semicolon.gspass.entity.school;
 
-public interface SchoolRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends CrudRepository<School, Integer> {
 }
