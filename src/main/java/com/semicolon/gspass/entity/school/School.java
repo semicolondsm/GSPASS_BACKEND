53a67634a05b6546862dcac8ece1291229586830
leeseojune <sung07288346@gmail.com>
2021-05-25 22:31:08 +0900

    ⚡ :: [School] 디비 모델 구현

--- src/main/java/com/semicolon/gspass/entity/school/School.java
+++ src/main/java/com/semicolon/gspass/entity/school/School.java
@@ -1,5 +1,38 @@
 package com.semicolon.gspass.entity.school;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Time;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class School {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45, nullable = false)
+    @NonNull
+    private String schoolCode;
+
+    @Column(length = 45, nullable = false)
+    @NonNull
+    private String scCode;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String randomCode;
+
+    private Time breakfastPeriod;
+
+    private Time lunchPeriod;
+
+    private Time dinnerPeriod;
+
+    @NonNull
+    private int timeLength;
 
 }
