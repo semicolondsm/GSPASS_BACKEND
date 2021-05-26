5b7b10678e4a8ec38a346b9de3aa8d494a2d58e4
leeseojune <sung07288346@gmail.com>
2021-05-26 16:03:28 +0900

    ⚙ :: [Test] GspassApplicationTests 기본 설정

--- src/test/java/com/semicolon/gspass/GspassApplicationTests.java
+++ src/test/java/com/semicolon/gspass/GspassApplicationTests.java
@@ -1,9 +1,17 @@
 package com.semicolon.gspass;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config.location=classpath:application-test.properties"})
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GspassApplicationTests {
 
     @Test
