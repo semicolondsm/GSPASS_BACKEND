96b6e27254975853b148a62462d5565503354180
leeseojune <sung07288346@gmail.com>
2021-05-26 11:06:29 +0900

    ⚡ :: [User] UserController 회원가입 추가

--- src/main/java/com/semicolon/gspass/controller/UserController.java
+++ src/main/java/com/semicolon/gspass/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.semicolon.gspass.controller;
 
+import com.semicolon.gspass.dto.user.RegisterRequest;
+import com.semicolon.gspass.dto.user.TokenResponse;
+import com.semicolon.gspass.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public TokenResponse register(@RequestBody RegisterRequest request) {
+        return userService.register(request);
+    }
+
 }
