35cb59b55178bab07ff0bbb16ff66df00f9e9d73
leeseojune <sung07288346@gmail.com>
2021-05-27 21:54:45 +0900

    ⚡ :: [School] 학교 정보 가져오기

--- src/main/java/com/semicolon/gspass/controller/SchoolController.java
+++ src/main/java/com/semicolon/gspass/controller/SchoolController.java
@@ -1,11 +1,14 @@
 package com.semicolon.gspass.controller;
 
 import com.semicolon.gspass.dto.school.MealResponse;
+import com.semicolon.gspass.dto.school.SchoolResponse;
 import com.semicolon.gspass.service.school.SchoolService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,6 +19,11 @@ public class SchoolController {
     @GetMapping("/meals")
     public MealResponse getMeals(@RequestParam("school_id") String schoolId, @RequestParam("date") String date) {
         return schoolService.getMeals(Integer.parseInt(schoolId), date);
+    }
+
+    @GetMapping("/school")
+    public List<SchoolResponse> getSchools(@RequestParam("name") String name) {
+        return schoolService.getSchools(name);
     }
 
 }
