94b8a7dd1be955e34bc47e510046f8d395e0a894
leeseojune <sung07288346@gmail.com>
2021-05-30 23:54:03 +0900

    ⚡ :: [RefreshToken] DAO에 RefreshToken으로 찾기 추가

--- src/main/java/com/semicolon/gspass/entity/refreshtoken/RefreshTokenRepository.java
+++ src/main/java/com/semicolon/gspass/entity/refreshtoken/RefreshTokenRepository.java
@@ -2,5 +2,8 @@ package com.semicolon.gspass.entity.refreshtoken;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String RefreshToken);
 }
