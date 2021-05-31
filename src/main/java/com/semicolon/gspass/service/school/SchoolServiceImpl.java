540a07aaf0556dd9d379ed1f5821c4f5758df4ae
leeseojune <sung07288346@gmail.com>
2021-05-31 21:46:15 +0900

    ♻ :: [School] 불필요한 import 삭제

--- src/main/java/com/semicolon/gspass/service/school/SchoolServiceImpl.java
+++ src/main/java/com/semicolon/gspass/service/school/SchoolServiceImpl.java
@@ -17,7 +17,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.transaction.Transactional;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
