0e31126c714201d9bf831094bea78d6b2745db66
leeseojune <sung07288346@gmail.com>
2021-05-31 09:30:09 +0900

    ⚡ :: [Security] resolve conflict

--- src/main/java/com/semicolon/gspass/security/JwtTokenProvider.java
+++ src/main/java/com/semicolon/gspass/security/JwtTokenProvider.java
@@ -1,12 +1,18 @@
-package com.semicolon.gspass.security.jwt;
+package com.semicolon.gspass.security;
 
+import com.semicolon.gspass.exception.InvalidTokenException;
+import com.semicolon.gspass.security.auth.AuthDetails;
 import com.semicolon.gspass.security.auth.AuthDetailsService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
 
@@ -55,7 +61,41 @@ public class JwtTokenProvider {
     }
 
     public boolean isRefreshToken(String token) {
-        return Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody().get("type").equals("refresh");
+        return getTokenBody(token).get("type").equals("refresh");
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader(header);
+        if(bearer != null && bearer.startsWith(prefix)) {
+            return bearer.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            return getTokenBody(token).getExpiration()
+                    .after(new Date());
+        }catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+
+    public Authentication authentication(String token) {
+        AuthDetails authDetails = authDetailsService.loadUserByUsername(getName(token));
+        return new UsernamePasswordAuthenticationToken(authDetails, "", authDetails.getAuthorities());
+    }
+
+    private String getName(String token) {
+        try {
+            return getTokenBody(token).getSubject();
+        }catch (Exception e) {
+            throw new InvalidTokenException();
+        }
+    }
+
+    private Claims getTokenBody(String token) {
+        return Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody();
     }
 
     private String getSigningKey() {
