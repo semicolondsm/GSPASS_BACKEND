e656b46236982e6b72e7f18006210f396aa3a221
leeseojune <sung07288346@gmail.com>
2021-05-31 19:17:56 +0900

    📑 :: [Teacher] 선생님 엔티티 구현

---
+++ src/main/java/com/semicolon/gspass/entity/teacher/Teacher.java
@@ -0,0 +1,30 @@
+package com.semicolon.gspass.entity.teacher;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.semicolon.gspass.entity.school.School;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Teacher {
+
+    @Id @Column(unique = true)
+    private String id;
+
+    @Column(length = 255)
+    private String password;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id")
+    @JsonBackReference
+    private School school;
+
+}
