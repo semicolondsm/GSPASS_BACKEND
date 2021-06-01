8bf154ae93914896d049f582bf5c9a3cc0b4f569
leeseojune <sung07288346@gmail.com>
2021-06-01 23:04:07 +0900

    ⚡ :: [School] Register Test 추가

--- src/test/groovy/com/semicolon/gspass/service/school/SchoolServiceImplTest.groovy
+++ src/test/groovy/com/semicolon/gspass/service/school/SchoolServiceImplTest.groovy
@@ -1,7 +1,9 @@
 package com.semicolon.gspass.service.school
 
+import com.semicolon.gspass.dto.school.RegisterRequest
 import com.semicolon.gspass.entity.school.SchoolRepository
 import com.semicolon.gspass.exception.ParseErrorException
+import com.semicolon.gspass.exception.SchoolAlreadyExistException
 import com.semicolon.gspass.facade.auth.AuthenticationFacade
 import spock.lang.Specification
 
@@ -17,9 +19,28 @@ class SchoolServiceImplTest extends Specification {
         schoolService.getSchools(name as String)
 
         then:
+
         notThrown ParseErrorException
 
         where:
         name << ["해강", "대덕", "부산"]
+    }
+
+    def "registerSchool"() {
+        given:
+        def schoolRepository = Mock(SchoolRepository)
+        def authenticationFacade = Mock(AuthenticationFacade)
+        SchoolService schoolService = new SchoolServiceImpl(schoolRepository, authenticationFacade)
+
+        when:
+        schoolService.registerSchool(new RegisterRequest(schoolCode, scCode))
+
+        then:
+        notThrown SchoolAlreadyExistException
+
+        where:
+        schoolCode | scCode
+        "7430310" | "G10"
+        "8521131" | "Q10"
     }
 }
