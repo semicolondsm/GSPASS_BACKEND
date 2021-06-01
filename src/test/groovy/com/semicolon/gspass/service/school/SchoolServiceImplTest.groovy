92fc129447f0f732337c1c6fe50f5a6e995f18a9
leeseojune <sung07288346@gmail.com>
2021-06-01 16:25:32 +0900

    ⚡ :: [Test] 수정

--- src/test/groovy/com/semicolon/gspass/service/school/SchoolServiceImplTest.groovy
+++ src/test/groovy/com/semicolon/gspass/service/school/SchoolServiceImplTest.groovy
@@ -2,6 +2,7 @@ package com.semicolon.gspass.service.school
 
 import com.semicolon.gspass.entity.school.SchoolRepository
 import com.semicolon.gspass.exception.ParseErrorException
+import com.semicolon.gspass.facade.auth.AuthenticationFacade
 import spock.lang.Specification
 
 class SchoolServiceImplTest extends Specification {
@@ -9,7 +10,8 @@ class SchoolServiceImplTest extends Specification {
     def "GetSchools"() {
         given:
         def schoolRepository = Mock(SchoolRepository)
-        SchoolService schoolService = new SchoolServiceImpl(schoolRepository)
+        def authenticationFacade = Mock(AuthenticationFacade)
+        SchoolService schoolService = new SchoolServiceImpl(schoolRepository, authenticationFacade)
 
         when:
         schoolService.getSchools(name as String)
