75ba00c7a3b0725b1c6fd0a54e016a5fe8472df2
leeseojune <sung07288346@gmail.com>
2021-06-02 12:15:15 +0900

    🧪 :: [Teacher] 관리자 등록, 로그인 테스트

---
+++ src/test/groovy/com/semicolon/gspass/service/teacher/TeacherServiceImplTest.groovy
@@ -0,0 +1,68 @@
+package com.semicolon.gspass.service.teacher
+
+import com.semicolon.gspass.dto.LoginRequest
+import com.semicolon.gspass.dto.teacher.RegisterRequest
+import com.semicolon.gspass.entity.refreshtoken.RefreshTokenRepository
+import com.semicolon.gspass.entity.school.School
+import com.semicolon.gspass.entity.school.SchoolRepository
+import com.semicolon.gspass.entity.teacher.Teacher
+import com.semicolon.gspass.entity.teacher.TeacherRepository
+import com.semicolon.gspass.exception.SchoolNotFoundException
+import com.semicolon.gspass.exception.TeacherNotFoundException
+import com.semicolon.gspass.facade.auth.AuthenticationFacade
+import com.semicolon.gspass.security.JwtTokenProvider
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import spock.lang.Specification
+
+class TeacherServiceImplTest extends Specification {
+
+    def teacherRepository = Mock(TeacherRepository)
+    def schoolRepository = Mock(SchoolRepository)
+    def refreshTokenRepository = Mock(RefreshTokenRepository)
+    def passwordEncoder = new BCryptPasswordEncoder()
+    def jwtTokenProvider = Mock(JwtTokenProvider)
+    def authenticationFacade = Mock(AuthenticationFacade)
+
+    def "RegisterTeacher"() {
+        given:
+        TeacherService teacherService = new TeacherServiceImpl(teacherRepository, schoolRepository,
+                refreshTokenRepository, passwordEncoder, jwtTokenProvider, authenticationFacade)
+
+        when:
+        teacherService.registerTeacher(new RegisterRequest(id, password, "testCode"))
+
+        then:
+        schoolRepository.findByRandomCode("testCode") >> Optional.of(new School(1, "7430310", "G10", null, null, null, null, 0, null, null))
+
+        notThrown SchoolNotFoundException
+
+        where:
+        id      | password
+        "test1" | "test1"
+        "test2" | "test2"
+
+    }
+
+    def "관리자 로그인"() {
+        given:
+        TeacherService teacherService = new TeacherServiceImpl(teacherRepository, schoolRepository,
+                refreshTokenRepository, passwordEncoder, jwtTokenProvider, authenticationFacade)
+
+        when:
+        teacherService.login(new LoginRequest(id, password))
+
+        then:
+        teacherRepository.findById(id) >> Optional.of(new Teacher(id, passwordEncoder.encode(password), null))
+
+        notThrown TeacherNotFoundException
+
+        where:
+        id | password
+        "test" | "test"
+        "test1" | "test1"
+    }
+
+    def "ChangePassword"() {
+    }
+
+}
