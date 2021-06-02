f4f889dace76e3b62f4232b8fcd6694dc3fcc7d0
leeseojune <sung07288346@gmail.com>
2021-06-02 17:00:42 +0900

    🧪 :: [User] 예외 테스트 추가

--- src/test/groovy/com/semicolon/gspass/service/user/UserServiceImplTest.groovy
+++ src/test/groovy/com/semicolon/gspass/service/user/UserServiceImplTest.groovy
@@ -11,6 +11,7 @@ import com.semicolon.gspass.entity.teacher.Teacher
 import com.semicolon.gspass.entity.user.User
 import com.semicolon.gspass.entity.user.UserRepository
 import com.semicolon.gspass.error.exception.GsException
+import com.semicolon.gspass.exception.InvalidPasswordException
 import com.semicolon.gspass.exception.InvalidTokenException
 import com.semicolon.gspass.exception.UserAlreadyExistException
 import com.semicolon.gspass.exception.UserNotFoundException
@@ -89,7 +90,35 @@ class UserServiceImplTest extends Specification {
         "test" | "테스터" | "1111111" | "0000" | "2020"    | "test"
     }
 
-    def "Login"() {
+    def "회원가입 아이디 중복"() {
+        given:
+        UserService userService = new UserServiceImpl(userRepository, schoolFacade, passwordEncoder
+                ,refreshTokenRepository , jwtTokenProvider, authenticationFacade)
+
+        when:
+        schoolRepository.save(
+                School.builder()
+                        .randomCode("test")
+                        .scCode("G10")
+                        .schoolCode("7430310")
+                        .timeLength(10)
+                        .build()
+        )
+        userService.register(new UserRegisterRequest(id, name, password, gcn, entryYear, randomCode))
+
+        then:
+        userRepository.findById(id) >> Optional.of(new User(id, name, passwordEncoder.encode(password), null, null, null))
+        userRepository.existsById(id) >> userRepository.findById(id).isPresent()
+
+        thrown UserAlreadyExistException
+
+        where:
+        id     | name  | password  | gcn    | entryYear | randomCode
+        "test" | "테스터" | "1111111" | "0000" | "2020"    | "test"
+
+    }
+
+    def "로그인"() {
         given:
         UserService userService = new UserServiceImpl(userRepository, schoolFacade, passwordEncoder
                 ,refreshTokenRepository , jwtTokenProvider, authenticationFacade)
@@ -101,6 +130,43 @@ class UserServiceImplTest extends Specification {
         userRepository.findById(id) >> Optional.of(new User(id, "test", passwordEncoder.encode(password), null, null, null))
 
         notThrown GsException
+
+        where:
+        id | password
+        "test" | "1111111"
+    }
+    
+    def "로그인 틀린 비밀번호"() {
+        given:
+        UserService userService = new UserServiceImpl(userRepository, schoolFacade, passwordEncoder
+                ,refreshTokenRepository , jwtTokenProvider, authenticationFacade)
+
+        when:
+        userService.login(new LoginRequest(id, "wrongPassword"))
+
+        then:
+        userRepository.findById(id) >> Optional.of(new User(id, "test", passwordEncoder.encode(password), null, null, null))
+
+        thrown GsException
+
+        where:
+        id | password
+        "test" | "1111111"
+    }
+
+    def "로그인 없는 유저"() {
+        given:
+        UserService userService = new UserServiceImpl(userRepository, schoolFacade, passwordEncoder
+                ,refreshTokenRepository , jwtTokenProvider, authenticationFacade)
+
+        when:
+        userService.login(new LoginRequest("wrongId", password))
+
+        then:
+        userRepository.findById(id) >> Optional.of(new User(id, "test", passwordEncoder.encode(password), null, null, null))
+        userRepository.findById("wrongId") >> Optional.empty()
+
+        thrown UserNotFoundException
 
         where:
         id | password
@@ -135,7 +201,27 @@ class UserServiceImplTest extends Specification {
         authenticationFacade.getUserId() >> 1.toString()
         userRepository.findById("1") >> Optional.of(new User("test", null, passwordEncoder.encode(oldPass), null, null, null))
 
-        notThrown UserNotFoundException
+        notThrown GsException
+
+        where:
+        oldPass | newPass
+        "1234" | "12345"
+        "test" | "test1"
+    }
+
+    def "비밀번호 변경 잘못된 비밀번호"() {
+        given:
+        UserService userService = new UserServiceImpl(userRepository, schoolFacade, passwordEncoder
+                ,refreshTokenRepository , jwtTokenProvider, authenticationFacade)
+
+        when:
+        userService.changePassword(new PasswordRequest(oldPass, "wrongPassword"))
+
+        then:
+        authenticationFacade.getUserId() >> 1.toString()
+        userRepository.findById("1") >> Optional.of(new User("test", null, passwordEncoder.encode(oldPass), null, null, null))
+
+        notThrown InvalidPasswordException
 
         where:
         oldPass | newPass
