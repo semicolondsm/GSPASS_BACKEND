e94dfc8ab892721a752a6c136489542bc8d88258
leeseojune <sung07288346@gmail.com>
2021-06-03 19:39:43 +0900

    ♻ :: [Teacher] 필요없는 import 삭제

--- src/main/java/com/semicolon/gspass/service/teacher/TeacherServiceImpl.java
+++ src/main/java/com/semicolon/gspass/service/teacher/TeacherServiceImpl.java
@@ -25,7 +25,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 
 @Service
