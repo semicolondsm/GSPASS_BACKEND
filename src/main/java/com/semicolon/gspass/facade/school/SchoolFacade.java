f725109e29edc2f72a8a2db9215b6f7bc646ae2f
leeseojune <sung07288346@gmail.com>
2021-06-03 19:16:42 +0900

    ♻ :: [User] User Facade로 통합

--- src/main/java/com/semicolon/gspass/facade/school/SchoolFacade.java
+++
@@ -1,7 +0,0 @@
-package com.semicolon.gspass.facade.school;
-
-import com.semicolon.gspass.entity.school.School;
-
-public interface SchoolFacade {
-    School findByRandomCode(String randomCode);
-}
