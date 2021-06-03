5011c24be00bf42ae1e9a0f862eaa28d0e4e7d28
leeseojune <sung07288346@gmail.com>
2021-06-03 23:21:50 +0900

    🐛 :: [Project] JsonNaming 변경

--- src/main/java/com/semicolon/gspass/dto/user/UserInformationResponse.java
+++ src/main/java/com/semicolon/gspass/dto/user/UserInformationResponse.java
@@ -1,5 +1,7 @@
 package com.semicolon.gspass.dto.user;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UserInformationResponse {
 
     private String schoolName;
