bf7615255a34ed7329449079f162287f0bf801ed
leeseojune <sung07288346@gmail.com>
2021-06-03 22:20:43 +0900

    📑 :: [User] 유저 정보 반환 DTO 추가

---
+++ src/main/java/com/semicolon/gspass/dto/user/UserInformationResponse.java
@@ -0,0 +1,15 @@
+package com.semicolon.gspass.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInformationResponse {
+
+    private String schoolName;
+    private String gcn;
+
+}
