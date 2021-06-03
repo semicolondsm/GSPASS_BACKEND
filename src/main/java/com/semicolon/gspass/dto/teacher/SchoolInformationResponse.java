c01905958d774785bb976d403847819737622054
leeseojune <sung07288346@gmail.com>
2021-06-03 23:23:01 +0900

    📑 :: [School] 학교 정보 반환 DTO추가

---
+++ src/main/java/com/semicolon/gspass/dto/teacher/SchoolInformationResponse.java
@@ -0,0 +1,18 @@
+package com.semicolon.gspass.dto.teacher;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class SchoolInformationResponse {
+
+    private String randomCode;
+    private String schoolName;
+
+}
