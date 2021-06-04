05f7e188986deede35556d2d494770a6ce390dc0
leeseojune <sung07288346@gmail.com>
2021-06-04 15:19:15 +0900

    ⚡ :: [User] 사용하지않은 급식패스 갯수, 급식패스 유저로 찾기 추가

--- src/main/java/com/semicolon/gspass/facade/user/UserFacade.java
+++ src/main/java/com/semicolon/gspass/facade/user/UserFacade.java
@@ -3,6 +3,7 @@ package com.semicolon.gspass.facade.user;
 import com.semicolon.gspass.entity.grade.Grade;
 import com.semicolon.gspass.entity.gspass.GsPass;
 import com.semicolon.gspass.entity.school.School;
+import com.semicolon.gspass.entity.user.User;
 
 import java.util.Optional;
 
@@ -11,4 +12,6 @@ public interface UserFacade {
     School findById(Integer id);
     GsPass save(GsPass gsPass);
     Optional<Grade> findByIdAndSchool(int id, School school);
+    int unUsedPassCount(Grade grade, int id);
+    Optional<GsPass> findByUser(User user);
 }
