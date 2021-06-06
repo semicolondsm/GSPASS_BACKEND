6e7517e05d6a28a57de14ffd693a812772ec0880
leeseojune <sung07288346@gmail.com>
2021-06-06 15:51:53 +0900

    ⚡ :: [GsPass] 나보다 앞에있는 모든 급식패스의 수

--- src/main/java/com/semicolon/gspass/entity/gspass/GsPassRepository.java
+++ src/main/java/com/semicolon/gspass/entity/gspass/GsPassRepository.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 public interface GsPassRepository extends CrudRepository<GsPass, Integer> {
     Optional<GsPass> findByUser(User user);
     int countByGradeAndIdLessThanAndUsed(Grade grade, int id, boolean isUsed);
+    int countByGradeAndIdLessThan(Grade grade, int id);
 }
