28edf0813162c684374d01126b01c5f9e6c34c13
leeseojune <sung07288346@gmail.com>
2021-06-07 09:41:14 +0900

    📑 :: [GsPass] 급식패스 시간 DTO

---
+++ src/main/java/com/semicolon/gspass/dto/user/GsPassTimeResponse.java
@@ -0,0 +1,16 @@
+package com.semicolon.gspass.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GsPassTimeResponse {
+
+    private Time gsPassTime;
+
+}
