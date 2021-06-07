7f0f28c447381992fff538b8ec3c8df751e364b4
leeseojune <sung07288346@gmail.com>
2021-06-07 20:25:41 +0900

    ⚡ :: [User] 로그인 DTO확인

--- src/main/java/com/semicolon/gspass/dto/LoginRequest.java
+++ src/main/java/com/semicolon/gspass/dto/LoginRequest.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
+    @NotEmpty
     private String id;
+    @NotEmpty
     private String password;
 
 }
