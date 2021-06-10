d750756f3005babe50fa38391b361c3342fdffe0
leeseojune <sung07288346@gmail.com>
2021-06-10 19:18:13 +0900

    📑 :: [Project] 스케쥴러 설정

---
+++ src/main/java/com/semicolon/gspass/config/ScheduledConfig.java
@@ -0,0 +1,19 @@
+package com.semicolon.gspass.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class ScheduledConfig {
+
+    @Bean
+    public TaskScheduler scheduler() {
+        var scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(4);
+        return scheduler;
+    }
+}
